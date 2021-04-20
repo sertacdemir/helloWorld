f2270214c1d5467c1faf81fb5d8a1f69bf2dc9f7
Sertaç Demir <sertacdemir@Sertacs-MacBook-Pro.local>
2021-04-20 16:52:16 +0200

    Listing the tasks

--- blockChain/ethTodoList/readme.txt
+++ blockChain/ethTodoList/readme.txt
@@ -11,7 +11,7 @@
 6. create package with correct versions
   6.1 npm view truffle-contract versions
 7. Create a TodoList contract under contracts
-  7.1 compile the project with truffle compile to check the contract
+  7.1 compile the project with 'truffle compile" to check the contract
 8. Edit truffle-config.js
   8.1 uncommend development
 9.Create a migration under Migrations
@@ -26,4 +26,19 @@
       10.3.3.1 Check the address: todoList.address
       10.3.3.2 Check the taskCount that is creates as state variable: todoList.taskCount() //should be zero
       10.3.3.3 examle: taskCount = await todoList.taskCount() and than check taskCount.toNumber()
-11.
+11.List tasks in contract TodoList
+  11.1. truffle compile: for compiling again
+  11.2. Deploy a new copy of the contract : truffle migrate --reset
+  11.3. check the initial list: truffle console
+    11.3.1. get copy of todoList: todoList = await TodoList.deployed()
+    11.3.2. to check get the first element: task = await todoList.tasks(1)
+    11.3.3. check the task: task
+12. Create client side application in src folder.
+  12.1. add web3 JavaScript part to the app.js
+  12.2. Web3 is added to package and app.js
+13. configure configuration of lite server to run as web server.
+  13.1. Create json file: bs-config.json
+14. Add ganage first account to metamask in chrome
+  14.1. change the port of the local network
+  14.2. import the account with private key
+  14.3. After the configuration we should see connected in refreshing the page
---
+++ blockChain/ethTodoList/src/app.js
@@ -0,0 +1,105 @@
+App = {
+  loading:false,
+  contracts:{},
+
+  load: async () => {
+    console.log("app is loading...")
+    await App.loadWeb3();
+    await App.loadAccount();
+    await App.loadContract();
+    await App.render();
+  },
+  // injecting web3 is not neceaasry anymore
+  loadWeb3: async () => {
+    const ethEnabled = () => {
+      if (window.ethereum) {
+        window.web3 = new Web3(window.ethereum);
+        window.ethereum.enable();
+        return true;
+      }
+      return false;
+    }
+  },
+
+  loadAccount: async() => {
+    App.account = await ethereum.request({ method: 'eth_accounts' });
+    console.log(App.account);
+  },
+
+  loadContract: async() => {
+    const todoList = await $.getJSON('TodoList.json');
+    App.contracts.TodoList = TruffleContract(todoList);
+    App.contracts.TodoList.setProvider(window.web3.currentProvider);
+
+    App.todoList = await App.contracts.TodoList.deployed();
+    console.log(App.todoList);
+  },
+
+  render: async() => {
+    //prevent double rendering
+    if(App.loading) {
+      return;
+    }
+    App.setLoading(true);
+
+    //render account
+    $('#account').html(App.account);
+    //renderTasks
+    await App.renderTasks();
+
+    App.setLoading(false);
+
+  },
+
+  renderTasks: async() => {
+    //Load the tasks from the block Blockchain
+    const taskCount = await App.todoList.taskCount();
+    const $taskTemplate = $('.taskTemplate');
+    console.log("Task number is " + taskCount);
+
+    //render each task
+    for(var i=1; i<=taskCount; i++) {
+      const task = await App.todoList.tasks(i);
+      const taskId = parseInt(task[0]);
+      const taskContent = task[1];
+      const taskCompleted = task[2];
+
+      const $newTaskTemplate = $taskTemplate.clone();
+      $newTaskTemplate.find('.content').html(taskContent);
+      $newTaskTemplate.find('input')
+                      .prop('name', taskId)
+                      .prop('checked', taskCompleted)
+                      //.on('click',App.toggleCompleted);
+      if(taskCompleted) {
+        $('#completedTaskList').append($newTaskTemplate);
+      } else {
+        $('#taskList').append($newTaskTemplate);
+      }
+
+      //show the task
+      $newTaskTemplate.show();
+    }
+  },
+
+  setLoading:(boolean) => {
+    App.loading = boolean;
+    const loader = $('#loader');
+    const content = $('#content');
+
+    if(boolean){
+      loader.show();
+      content.hide();
+    } else {
+      loader.hide();
+      content.show();
+    }
+  }
+}
+
+
+
+$(() => {
+  $(window).load(() => {
+    App.load()
+  })
+})
