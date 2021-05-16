ac33c600f4ac32c777e94e329d85115f287b7228
Sertaç Demir <sertacdemir@Sertacs-MacBook-Pro.local>
2021-05-16 12:17:14 +0200

    story app is completed with edit and delete

---
+++ nodejs/storyApp/models/Story.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+
+const StorySchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    body: {
+        type: String,
+        required: true
+    },
+    status: {
+        type: String,
+        defauls: 'public',
+        enum: ['public', 'private']
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        required: true
+    }
+})
+
+module.exports = mongoose.model('Story', StorySchema)
--- nodejs/storyApp/readme.txt
+++ nodejs/storyApp/readme.txt
@@ -3,6 +3,11 @@ Based on : https://www.youtube.com/watch?v=SBvmnHTQIPY&list=WL&index=27
 Some resources used:
 https://materializecss.com/getting-started.html
 https://cdnjs.com/libraries/font-awesome
+https://cdnjs.com/libraries/ckeditor
 http://www.passportjs.org/docs/
 http://www.passportjs.org/packages/passport-google-oauth20/
 https://www.npmjs.com/package/express-session
+https://www.npmjs.com/package/method-override
+
+For authentication:
+https://console.cloud.google.com/
